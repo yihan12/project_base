f9327b5a5b39b56159df41fff94701e75791a3c9
yihan12 <Pengyaohui6666.outlook.com>
2021-10-25 17:29:02 +0800

    111

---
+++ vant-pro/api/login.js
@@ -0,0 +1,8 @@
+import { loginIn } from "./index";
+const feature = "auth";
+
+const thirdLogin = `/${loginIn}/${feature}/thirdLogin`;
+
+module.exports = {
+  thirdLogin,
+};
---
+++ vant-pro/libs/utils/help.js
@@ -0,0 +1,2 @@
+export const publicKey =
+  "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAN6uj93xw48P9snqi2K/wEPcWj/DDcnYV4uUupjFSeRNubeRc6SQ1NpWfVkXNteZnyjRw5eTZNv5VK3GNZ/c9AECAwEAAQ=="
