1a959ef9d549a6192a38728bb997b32b9118264c
yihan12 <Pengyaohui6666.outlook.com>
2021-10-27 11:25:47 +0800

    常量mutation

--- exercise_vue3.0/src/store/index.js
+++ exercise_vue3.0/src/store/index.js
@@ -2,9 +2,13 @@ import { createStore } from 'vuex'
 
 import modules from './modules'
 
+import * as types from "./mutation_types"
+
 
 // 在开发环境中开启logger
 const debug = process.env.NODE_ENV !== 'production'
+
+console.log('types: ----', [types.TABS_ROUTES]);
 
 export default createStore({
     modules,
