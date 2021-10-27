3e210b6435cc88300cec30cfd4f84e55e16ded28
yihan12 <Pengyaohui6666@outlook.com>
2021-10-27 13:22:00 +0800

    提交

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git filter-branch --env-filter '
+OLD_EMAIL="Pengyaohui6666outlook.com"
+CORRECT_NAME="yihan12"
+CORRECT_EMAIL="Pengyaohui6666@outlook.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
