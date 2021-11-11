f99b9b0ddc3ba031eeb50790cf82acab78be7d76
yihan12 <Pengyaohui6666@outlook.com>
2021-11-11 17:33:11 +0800

    安装less

--- exercise_vue3.0/src/App.vue
+++ exercise_vue3.0/src/App.vue
@@ -12,13 +12,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+@import './assets/css/public.less';
 </style>
