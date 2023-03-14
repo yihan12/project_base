ae6f7c9ff5552e00b70f042e507bbaec6c1feb7a
yihan12 <Pengyaohui6666@outlook.com>
2023-03-14 23:07:18 +0800

    new init 执行了什么?

--- vue2.6/src/core/instance/init.js
+++ vue2.6/src/core/instance/init.js
@@ -13,6 +13,8 @@ import { extend, mergeOptions, formatComponentName } from '../util/index'
 let uid = 0
 
 // init
+// 把不同的功能逻辑拆成一些单独的函数执行，让主线逻辑一目了然
+// 在初始化最后，检测是否还有el属性，则调用vm.$mount方法挂载vm,挂载的目标就是把模板渲染成最终的 DOM
 export function initMixin (Vue: Class<Component>) {
   Vue.prototype._init = function (options?: Object) {
     const vm: Component = this
@@ -29,7 +31,7 @@ export function initMixin (Vue: Class<Component>) {
 
     // a flag to avoid this being observed
     vm._isVue = true
-    // merge options
+    // merge options合并options配置
     if (options && options._isComponent) {
       // optimize internal component instantiation
       // since dynamic options merging is pretty slow, and none of the
@@ -50,23 +52,31 @@ export function initMixin (Vue: Class<Component>) {
     }
     // expose real self
     vm._self = vm
+    // 初始化生命周期
     initLifecycle(vm)
+    // 初始化事件中心
     initEvents(vm)
+    // 初始化渲染
     initRender(vm)
+    /*调用beforeCreate钩子函数并且触发beforeCreate钩子事件*/
     callHook(vm, 'beforeCreate')
     initInjections(vm) // resolve injections before data/props
+    /*初始化props、methods、data、computed与watch*/
     initState(vm)
     initProvide(vm) // resolve provide after data/props
+    /*调用created钩子函数并且触发created钩子事件*/
     callHook(vm, 'created')
 
     /* istanbul ignore if */
     if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
+      // 格式化组件名
       vm._name = formatComponentName(vm, false)
       mark(endTag)
       measure(`vue ${vm._name} init`, startTag, endTag)
     }
     // 判断options参数里是否有el
     if (vm.$options.el) {
+      // 挂载组件
       vm.$mount(vm.$options.el)
     }
   }
